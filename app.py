6991c98abc6919349600ded392c30d561a205bf3
Sunand Pradhan <sunandd92@gmail.comgit config --global user.email sunandd92@gmail.comclear>
2024-04-13 00:48:24 +0530

    final commit

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from dotenv import load_dotenv
-load_dotenv()
-
 import streamlit as st
 import os
 from PIL import Image
 import google.generativeai as genai
+
+from dotenv import load_dotenv
+load_dotenv()
 
 genai.configure(api_key = os.getenv("GOOGLE_API_KEY"))
 
